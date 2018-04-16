2eeabefa07f72c471cee96a803f93482cbd524bd
Renato Santana <renatod.santanajr@gmail.com>
2018-04-16 00:14:40 -0300

    função de autenticação

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
  
-// SHOW LIST OF USERS
+// Lista de usuários
 app.get('/', function(req, res, next) {
     req.getConnection(function(error, conn) {
         conn.query('SELECT * FROM users ORDER BY nome DESC',function(err, rows, fields) {
@@ -23,7 +23,6 @@ app.get('/', function(req, res, next) {
     })
 })
 
-// SHOW ADD USER FORM
 app.get('/add', function(req, res, next){    
     // render to views/user/add.ejs
     res.render('user/add', {
@@ -39,22 +38,11 @@ app.post('/add', function(req, res, next){
  
     var errors = req.validationErrors()
     
-    if( !errors ) {   //No errors were found.  Passed Validation!
-        
-        /********************************************
-         * Express-validator module
-         
-        req.body.comment = 'a <span>comment</span>';
-        req.body.username = '   a user    ';
- 
-        req.sanitize('comment').escape(); // returns 'a &lt;span&gt;comment&lt;/span&gt;'
-        req.sanitize('username').trim(); // returns 'a user'
-        ********************************************/
+    if( !errors ) {  
         var user = {
             nome: req.sanitize('nome').escape().trim(),
             senha: req.sanitize('senha').escape().trim()
         }
-        
         req.getConnection(function(error, conn) {
             conn.query('INSERT INTO users SET ?', user, function(err, result) {
                 //if(err) throw err
@@ -80,7 +68,7 @@ app.post('/add', function(req, res, next){
             })
         })
     }
-    else {   //Display errors to user
+    else {   
         var error_msg = ''
         errors.forEach(function(error) {
             error_msg += error.msg + '<br>'
@@ -96,41 +84,47 @@ app.post('/add', function(req, res, next){
 })
 
 
-app.post('/login', function(req,res){
+app.get('/login', function(req,res){
     var nome= req.body.nome;
     var senha = req.body.senha;
 
-    connection.query('SELECT * FROM users WHERE nome = ?',[nome], function (error, results, fields) {
-    if (error) {
-      // console.log("error ocurred",error);
-      res.send({
-        "code":400,
-        "failed":"error ocurred"
-      })
-    }else{
-      // console.log('The solution is: ', results);
-      if(results.length >0){
-        if([0].nome == nome){
-          res.send({
-            "code":200,
-            "success":"login sucessfull"
-              });
-        }
-        else{
-          res.send({
-            "code":204,
-            "success":"Email and password does not match"
-              });
-        }
-      }
-      else{
-        res.send({
-          "code":204,
-          "success":"Email does not exits"
-            });
-      }
-    }
-    });
+    req.getConnection(function(error, conn){
+
+        conn.query('SELECT * FROM users WHERE nome = ? and senha = ?',[nome],[senha], function (error, results, fields) {
+            if (error) {
+              console.log("error ocurred",error);
+              res.send({
+                "code":400,
+                "failed":"error ocurred"
+              })
+            }else{
+        
+              if(results.length >0){
+                if([0].nome == nome){
+                  res.send({
+                    "code":200,
+                    "success":"login sucessfull"
+                      });
+                }
+                else{
+                  res.send({
+                    "code":204,
+                    "success":"Email and password does not match"
+                      });
+                }
+              }
+              else{
+                res.send({
+                  "code":204,
+                  "success":"Email does not exits"
+                    });
+              }
+            }
+        })
+    })
+
+
+    
   })
 
 
