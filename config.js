ba5f6c47b7fea95cd47c130b0ecf813dff93e8b0
Renato Santana <renatod.santanajr@gmail.com>
2018-04-16 09:16:38 -0300

    qwef

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ var config = {
         db:       'users'         
     },
     server: {
-        host: 'https://testeappaut.herokuapp.com/',
+        host: '127.0.0.1',
         port: '3000'
     }
 }
